c7e7e21b0fd83546fe1b0026616f86b95f2487aa
Breyner Sánchez <breinersanchez97@hotmail.com>
2024-03-03 18:54:37 -0400

    exercise 5

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -28,15 +28,44 @@ function Game() {
     if ( Object.values( results ).includes( value ) ) return;
 
     const guessResult = checkGuess( value, answer );
-    const nextResults = { ...results, [ crypto.randomUUID() ]: guessResult };
+    const nextResults = { ...results, [ crypto.randomUUID() ]: [ ...guessResult ] };
     setResults( nextResults );
     setWordValue( '' );
   }
+
+  const isLose = ( valuestoCheck ) => {
+    const resultValeus = Object.values( valuestoCheck );
+    if ( resultValeus.length !== NUM_OF_GUESSES_ALLOWED ) return false;
+
+    const [ _, label ]=resultValeus[ resultValeus.length - 1 ];
+    return label.status !== 'correct'
+  }
+
+  const isWin = ( valuesToCheck ) => {
+    const resultValues = Object.values( valuesToCheck );
+    const valuesJoined = resultValues.map(( result ) => result.map(({ letter }) => letter ).join(''));
+
+    if ( valuesJoined.includes( answer ) ) return true;
+  }
+
+
 
   return (
     <>
       <GuessResults results={Object.entries(results)}/>
       <InputWord inputValue={ wordValue } onChange={ onInputValueChange } onSubmit={ onInputSubmit } />
+
+      { Object.keys(results).length > 5 && isLose( results ) && (
+        <div class="sad banner">
+          <p>Sorry, the correct answer is <strong>{answer}</strong>.</p>
+        </div>
+      )}
+
+      { isWin( results ) && (
+        <div className="happy banner">
+          <p>Congratulations! You've won!</p>
+        </div>
+      )}
     </>
   )
 }
